f1ece11d61434024424483d4b8ed87d9349b4862
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-06-01 15:26:45 +0200

    better comics titles style

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -7,124 +7,130 @@ use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $comics = Comic::all();
-        return view('comics', compact('comics'));
-    }
- 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('admin.comics.create');
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index(Request $request)
+  {
+    $comics = Comic::paginate(12);
+
+    if ($request->ajax()) {
+      $view = view('child', compact('comics'))->render();
+      return response()->json(['html' => $view]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $val_data = $request->validate([
-            'title' => 'required|max:200',
-            'description' => 'nullable|max:1000',
-            'thumb' => 'nullable|max:255',
-            'price' => 'nullable|max:50',
-            'sale_date' => 'nullable|max:50',
-            'type' => 'nullable|max:50',
-        ]);
+    return view('comics', compact('comics'));
+  }
 
-        // dd($request->all());
-        // $data = [
-        //     'title' => $request->title,
-        //     'description' => $request->description,
-        //     'thumb' => $request->thumb,
-        //     'price' => $request->price,
-        //     'sale_date' => $request->sale_date,
-        //     'type' => $request->type,
-        // ];
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    return view('admin.comics.create');
+  }
 
-        Comic::create($val_data);
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $val_data = $request->validate([
+      'title' => 'required|max:200',
+      'description' => 'nullable|max:1000',
+      'thumb' => 'nullable|max:255',
+      'price' => 'nullable|max:50',
+      'sale_date' => 'nullable|max:50',
+      'type' => 'nullable|max:50',
+    ]);
 
-        return to_route('admin.index')->with('message', 'Comic created');
-    }
+    // dd($request->all());
+    // $data = [
+    //     'title' => $request->title,
+    //     'description' => $request->description,
+    //     'thumb' => $request->thumb,
+    //     'price' => $request->price,
+    //     'sale_date' => $request->sale_date,
+    //     'type' => $request->type,
+    // ];
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Comic $comic
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Comic $comic)
-    {
-        return view('admin.comics.show', compact('comic'));
-    }
+    Comic::create($val_data);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Comic $comic
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Comic $comic)
-    {
-        return view('admin.comics.edit', compact('comic'));
-    }
+    return to_route('admin.index')->with('message', 'Comic created');
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Comic $comic
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Comic $comic)
-    {
-        $val_data = $request->validate([
-            'title' => 'required|max:200',
-            'description' => 'nullable|max:1000',
-            'thumb' => 'nullable|max:255',
-            'price' => 'nullable|max:50',
-            'sale_date' => 'nullable|max:50',
-            'type' => 'nullable|max:50',
-        ]);
-        
-        //dd($request->all());
-        // $data = [
-        //     'title' => $request->title,
-        //     'description' => $request->description,
-        //     'thumb' => $request->thumb,
-        //     'price' => $request->price,
-        //     'sale_date' => $request->sale_date,
-        //     'type' => $request->type,
-        // ];
-        //dd($data);
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Models\Comic $comic
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Comic $comic)
+  {
+    return view('admin.comics.show', compact('comic'));
+  }
 
-        $comic->update($val_data);
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Models\Comic $comic
+   * @return \Illuminate\Http\Response
+   */
+  public function edit(Comic $comic)
+  {
+    return view('admin.comics.edit', compact('comic'));
+  }
 
-        return to_route('admin.index')->with('message', 'Comic updated');
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Models\Comic $comic
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, Comic $comic)
+  {
+    $val_data = $request->validate([
+      'title' => 'required|max:200',
+      'description' => 'nullable|max:1000',
+      'thumb' => 'nullable|max:255',
+      'price' => 'nullable|max:50',
+      'sale_date' => 'nullable|max:50',
+      'type' => 'nullable|max:50',
+    ]);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Comic $comic
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Comic $comic)
-    {
-        $comic->delete();
-        return to_route('admin.index')->with('message', 'Comic deleted'); 
-    }
+    //dd($request->all());
+    // $data = [
+    //     'title' => $request->title,
+    //     'description' => $request->description,
+    //     'thumb' => $request->thumb,
+    //     'price' => $request->price,
+    //     'sale_date' => $request->sale_date,
+    //     'type' => $request->type,
+    // ];
+    //dd($data);
+
+    $comic->update($val_data);
+
+    return to_route('admin.index')->with('message', 'Comic updated');
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Models\Comic $comic
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Comic $comic)
+  {
+    $comic->delete();
+    return to_route('admin.index')->with('message', 'Comic deleted');
+  }
 }
--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -2,31 +2,33 @@
 
 @section('content')
     <div id="banner"></div>
-<div class="main_top">
-  <div class="container py-5">
-      <div class="d-flex justify-content-between align-items-center">
-        <div id="title">
-          <h3>CURRENT SERIES</h3>
+    <div class="main_top">
+        <div class="container py-5">
+            <div class="d-flex justify-content-between align-items-center">
+                <div id="title">
+                    <h3>CURRENT SERIES</h3>
+                </div>
+                <div>
+                    <a class="btn btn-outline-light" href="{{ route('admin.index') }}">Admin View</a>
+                </div>
+            </div>
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-6 py-5">
+                @forelse ($comics as $comic)
+                    <div class="col">
+                        <img src="{{ $comic->thumb }}" class="img-fluid rounded-start" alt="{{ $comic->title }}">
+                        <div class="my-2">
+                            <a id="titles" href="{{ route('admin.comics.show', $comic->id) }}">{{ $comic->title }}</a>
+                        </div>
+                    </div>
+                @empty
+                    <tr scope="row">
+                        <td>No Comics found</td>
+                    </tr>
+                @endforelse
+            </div>
+            <div class="d-flex justify-content-center pb-4">
+                <button type="button" class="btn btn-primary" id="load-more-btn" data-page="1">Load More</button>
+            </div>
         </div>
-        <div>
-          <a class="btn btn-outline-light" href="{{ route('admin.index') }}">Admin View</a>
-        </div>
-      </div>
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-6 py-5">
-          @forelse ($comics as $comic)
-              <div class="col">
-                  <img src="{{ $comic->thumb }}" class="img-fluid rounded-start" alt="{{ $comic->title }}">
-                  <a href="{{ route('admin.comics.show', $comic->id) }}" class="mt-2">{{ $comic->title }}</a>
-              </div>
-          @empty
-              <tr scope="row">
-                  <td>No Comics found</td>
-              </tr>
-          @endforelse
-      </div>
-      <div class="d-flex justify-content-center pb-4">
-        <button type="button" class="btn btn-primary">Load More</button>
-      </div>
-  </div>
-</div>
+    </div>
 @endsection
